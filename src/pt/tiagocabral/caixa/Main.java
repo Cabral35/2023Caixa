797fc08984a38f323be66206a66b319b20af4aa3
tiago <tiago@DESKTOP-CTTGAQ7>
2023-03-01 14:16:34 +0000

    .

--- src/pt/tiagocabral/caixa/Main.java
+++ src/pt/tiagocabral/caixa/Main.java
@@ -3,8 +3,11 @@ package pt.tiagocabral.caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("******");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("*    *");
+		System.out.println("******");
 	}
 
 }
