fab60c8db57477d71a790376793487dededd59f7
tiago <tiago@DESKTOP-CTTGAQ7>
2023-03-01 14:41:28 +0000

    Parametrizar printTopOrBottom

--- src/pt/tiagocabral/caixa/Main.java
+++ src/pt/tiagocabral/caixa/Main.java
@@ -3,16 +3,18 @@ package pt.tiagocabral.caixa;
 public class Main {
 
 	public static void main(String[] args) {
-		printTopOrBottom();
-		System.out.println("******");
+		printTopOrBottom(4);
 		System.out.println("*    *");
 		System.out.println("*    *");
 		System.out.println("*    *");
-		System.out.println("******");
+		printTopOrBottom(4);
 	}
 	
-	public static void printTopOrBottom(){
-		System.out.println("******");
+	public static void printTopOrBottom(int size){
+		for( int i = 0; i < size; i++) {
+		System.out.print("*");
+		}
+		System.out.println();
 	}
 
 }
