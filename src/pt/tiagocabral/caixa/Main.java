7bf6a38df50f6b729c5f99de1771c9c85a81ace1
tiago <tiago@DESKTOP-CTTGAQ7>
2023-03-01 14:31:09 +0000

    Criar printTopOrBottom

--- src/pt/tiagocabral/caixa/Main.java
+++ src/pt/tiagocabral/caixa/Main.java
@@ -3,10 +3,15 @@ package pt.tiagocabral.caixa;
 public class Main {
 
 	public static void main(String[] args) {
+		printTopOrBottom();
 		System.out.println("******");
 		System.out.println("*    *");
 		System.out.println("*    *");
 		System.out.println("*    *");
+		System.out.println("******");
+	}
+	
+	public static void printTopOrBottom(){
 		System.out.println("******");
 	}
 
